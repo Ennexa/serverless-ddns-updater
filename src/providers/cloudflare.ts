a7f400a464a84f89eb5e22c346ad1fd5da620954
Joyce Babu <joyce@ennexa.com>
2021-05-04 19:50:50 +0530

    fix: cloudflare client is not working in lambda

--- src/providers/cloudflare.ts
+++ src/providers/cloudflare.ts
@@ -1,18 +1,79 @@
 'use strict';
 
-import Cloudflare from 'cloudflare';
+import https from 'https';
 
 class CloudflareClient {
-    private client: Cloudflare;
     private hostedZoneId: null|string|Promise<string>;
+    private token:string;
 
     constructor(config: any, zoneId: string|null = null) {
         if (!('token' in config)) {
             throw new Error('Missing key `token` in cloudflare provider config');
         }
 
-        this.client = new Cloudflare({ token : config.token });
+        this.token = config.token;
         this.hostedZoneId = zoneId;
+    }
+
+    public apiRequest(path: string, payload: any = null, method: string = 'GET'): any {
+        const headers = {
+            'Authorization': `Bearer ${this.token}`,
+        };
+        const request = {
+            host: `api.cloudflare.com`,
+            port: '443',
+            path: `/client/v4/${path}`,
+            method: method,
+            headers: headers
+        };
+
+        const isGet = method === 'GET';
+        let requestBody:any;
+
+        if (!isGet) {
+            requestBody = JSON.stringify(payload);
+            Object.assign(headers, {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(requestBody)
+            });
+        }
+
+        return new Promise((resolve, reject) => {
+            const req = https.request(request, res => {
+                res.setEncoding("utf8");
+                let responseBody:any = "";
+
+                res.on("data", data => {
+                    responseBody += data;
+                });
+
+                res.on("end", () => {
+                    responseBody = JSON.parse(responseBody);
+                    const isSuccess = responseBody.success === true;
+
+                    if (!isSuccess && responseBody.error_chain) {
+                        for (let error of responseBody.error_chain) {
+                            console.error(`[${error.code}] ${error.message}`);
+                        }
+                    }
+
+                    if (res.statusCode !== 200 || !isSuccess) {
+                        reject(responseBody.errors[0].message || 'API Request failed');
+
+                        return;
+                    }
+
+                    resolve(responseBody);
+                });
+
+                res.on('error', reject);
+            });
+
+            if (!isGet && requestBody) {
+                req.write(requestBody);
+            }
+            req.end();
+        });
     }
 
     private getHostedZoneId(hostname: string): string|Promise<string> {
@@ -25,7 +86,8 @@ class CloudflareClient {
 
     private async findHostedZoneId(hostname: string) {
         try {
-            let response:any = await this.client.zones.browse();
+            // let response:any = await this.client.zones.browse();
+            let response:any = await this.apiRequest('zones');
 
             if (!response.success) {
                 throw new Error('API Request failed.');
@@ -44,7 +106,8 @@ class CloudflareClient {
     }
 
     private async getRecord(zoneId: string, name: string, type: string): Promise<any|null> {
-        let response:any = await this.client.dnsRecords.browse(zoneId);
+        // let response:any = await this.client.dnsRecords.browse(zoneId);
+        let response:any = await this.apiRequest(`zones/${zoneId}/dns_records`);
 
         if (!response.success) {
             throw new Error('API Request failed.');
@@ -62,17 +125,18 @@ class CloudflareClient {
     }
 
     private updateRecord(zoneId: string, recordId: string, name: string, value: string, type: string, ttl: number) {
-        return this.client.dnsRecords.edit(
-            zoneId,
-            recordId,
-            {name, type: type as Cloudflare.RecordTypes, ttl: ttl, content: value, proxied: false}
+        return this.apiRequest(
+            `zones/${zoneId}/dns_records/${recordId}`,
+            {name, type: type, ttl: ttl, content: value, proxied: false},
+            'PATCH'
         );
     }
 
     private createRecord(zoneId: string, name: string, value: string, type: string, ttl: number) {
-        return this.client.dnsRecords.add(
-            zoneId,
-            {name, type: type as Cloudflare.RecordTypes, ttl: ttl, content: value, proxied: false, priority: 10}
+        return this.apiRequest(
+            `zones/${zoneId}/dns_records`,
+            {name, type: type, ttl: ttl, content: value, proxied: false, priority: 10},
+            'POST'
         );
     }
 
